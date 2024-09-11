c798ac97e6702be935938f0826e6857211d0a5ee
Dmitry Tarasov <brahmanchik@gmail.com>
2024-09-11 11:05:51 +0300

    step 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/brahmanchik/php-project-lvl1/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/brahmanchik/php-project-lvl1/actions)
+
+<a href="https://codeclimate.com/github/brahmanchik/php-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/3211c6a3660391d72fc1/maintainability" /></a>
--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,27 @@
 <?php
 
+/**
+ * Этот файл содержит функции для работы с Brain Games.
+ *
+ * @category    CLI
+ * @package     BrainGames
+ * @author      brahmanchik <brahmanchik@gmail.com>
+ * @license     MIT License <https://opensource.org/licenses/MIT>
+ * @version     GIT: <2.45>
+ * @link        https://example.com
+ * @php_version 8.3.11
+ */
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * Функция выводит приветствие и запрашивает имя пользователя.
+ *
+ * @return void
+ */
 function getName()
 {
     line('Welcome to the Brain Games!');
