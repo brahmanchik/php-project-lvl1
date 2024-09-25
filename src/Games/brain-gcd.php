83c78ff3bf2d108e2a7576ceeeeae53d5721c6fd
Dmitry Tarasov <brahmanchik@gmail.com>
2024-09-25 20:52:58 +0300

    refactoring 1.1

--- src/Games/brain-gcd.php
+++ src/Games/brain-gcd.php
@@ -11,12 +11,22 @@ use function BrainGames\Engine\counter;
 use function BrainGames\Engine\playRound;
 use function BrainGames\Engine\greeting;
 
+//Функция использующая алгоритм Евклида, чтобы найти НОД
+function gcd($a, $b) {
+    while ($b != 0) {
+        $temp = $b;
+        $b = $a % $b;
+        $a = $temp;
+    }
+    return $a;
+}
 function gameLogic(): array
 {
-    //генерирую случайное математическое выражение вида 2 + 2
+    //показывается два случайных числа, например, 25 50.
+    //Пользователь должен вычислить и ввести наибольший общий делитель этих чисел.
     $numberOne = rand(1, 100);
     $numberTwo = rand(1, 100);
-    $correctAnswer = strval(gmp_gcd($numberOne, $numberTwo)); // Наибольший общий делитель
+    $correctAnswer = strval(gcd($numberOne, $numberTwo)); // Наибольший общий делитель
     $question = "{$numberOne} {$numberTwo}";
     return [$question, $correctAnswer]; //$возвращаю массив из 2 элементов, выражение и ответа на это выражение
 }
